ef943adc7539c69108b8d0d59367c97add7b9667
“WillGoadgit config --global user.email “goad.w@etoncollege.org.uk <goad.w@etoncollege.org.uk>
2020-01-14 12:23:27 +0000

    initial check-in of tamagotchi app

---
+++ TamagotchiApp/TamagotchiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TamagotchiApp
+//
+//  Created by Goad, William (PAH) on 14/01/2020.
+//  Copyright © 2020 Goad, William (PAH). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
